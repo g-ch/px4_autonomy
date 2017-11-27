9b5024e2f6cb4185da4426f59e40dcd4e8c36a53
Jister <chenjiesjtu14@gmail.com>
2017-11-27 21:36:34 +0800

    good

--- src/ps2.cpp
+++ src/ps2.cpp
@@ -50,6 +50,9 @@ float cmd_vy = 0.0;
 float cmd_vz = 0.0;
 float cmd_yr = 0.0;
 
+float obstecle_distance = 100.0;
+float vel_scale = 1.0;
+
 px4_autonomy::Velocity ps2_vel;
 px4_autonomy::Velocity body_vel;
 
@@ -100,6 +103,24 @@ void chatterCallback_joy(const sensor_msgs::Joy::ConstPtr& joy)
 
 }
 
+void chatterCallback_obstacle_dist(const std_msgs::Float32 &msg)
+{
+    obstecle_distance = msg.data;
+    if(obstecle_distance > 5.0) vel_scale = 1.0;
+    else if(obstecle_distance > 1.5) vel_scale = 0.7 - (5.0 - obstecle_distance) * 0.2;
+    else vel_scale = 0.0; 
+}
+
+float vec_length(float x, float y, float z)
+{
+    return sqrt(x*x + y*y + z*z);
+}
+
+float vec_length_2d(float x, float y)
+{
+    return sqrt(x*x + y*y);
+}
+
 int main(int argc, char **argv)
 {
     /* code for main function */
@@ -127,6 +148,7 @@ int main(int argc, char **argv)
     ros::Subscriber local_pose_sub = nh.subscribe("/px4/pose", 1,chatterCallback_pose);
     ros::Subscriber local_vel_sub = nh.subscribe("/px4/velocity", 1,chatterCallback_vel);
     ros::Subscriber joy_sub = nh.subscribe("/joy",1,chatterCallback_joy);
+    ros::Subscriber obstecle_sub = nh.subscribe("/obstacle_dist", 1, chatterCallback_obstacle_dist);
 
     ros::Publisher pose_pub = nh.advertise<px4_autonomy::Position>("/px4/cmd_pose", 1); 
     ros::Publisher vel_pub = nh.advertise<px4_autonomy::Velocity>("/px4/cmd_vel", 1);
@@ -167,6 +189,34 @@ int main(int argc, char **argv)
             }
         }
 
+        /*Two Direction for Obstecle Avoidance*/
+        float current_v = vec_length_2d(current_vx, current_vy); 
+        float control_v = vec_length(cmd_vx, cmd_vy, cmd_vz);
+
+        if(current_v < 0.05 && control_v < 0.01) //avoid noise when hovering
+        {
+            ps2_vel.x = 1.f;
+            ps2_vel.y = 0.f;
+            ps2_vel.z = 0.f;
+
+            body_vel.x = 1.f;
+            body_vel.y = 0.f;
+            body_vel.z = 0.f;
+        }
+        else if(current_v < 0.05)
+        {
+            body_vel.x = ps2_vel.x;
+            body_vel.y = ps2_vel.y;
+            body_vel.z = ps2_vel.z;
+        }
+        else if(control_v < 0.01)
+        {
+            ps2_vel.x = body_vel.x;
+            ps2_vel.y = body_vel.y;
+            ps2_vel.z = body_vel.z;
+        }
+
+        /*Control Status*/
         switch(status)
         {
             case 1:
@@ -185,8 +235,8 @@ int main(int argc, char **argv)
                 if(ps2_flag == FLY)
                 {
                     vel.header.stamp = ros::Time::now();
-                    vel.x = cmd_vx*cos(current_yaw) - cmd_vy*sin(current_yaw);
-                    vel.y = cmd_vx*sin(current_yaw) + cmd_vy*cos(current_yaw);
+                    vel.x = (cmd_vx*cos(current_yaw) - cmd_vy*sin(current_yaw)) * vel_scale;
+                    vel.y = (cmd_vx*sin(current_yaw) + cmd_vy*cos(current_yaw)) * vel_scale;
                     vel.z = cmd_vz;
                     vel.yaw_rate = cmd_yr;
                     vel_pub.publish(vel);
